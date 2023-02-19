cb92683b1bff9ede4253cedab1fc82a53bc2dc71
Basualdo Joel <basualdojjoel@gmail.com>
2023-02-19 18:31:28 -0300

    2° Commit

--- Primera Entrega - Funciones/js/main.js
+++ Primera Entrega - Funciones/js/main.js
@@ -1,51 +1,49 @@
-// Definimos las variables necesarias
-var ancho = 0;
-var alto = 0;
-var tipo = "";
-var precioPorMetroCuadrado = 0;
-var metrosCuadrados = 0;
-var precioTotal = 0;
-var descuento = 0;
-var cuotas = 0;
-var interes = 0;
+function calcularPrecioTotal() {
+  // Definimos las variables necesarias
+  var ancho = parseFloat(document.getElementById("ancho").value);
+  var alto = parseFloat(document.getElementById("alto").value);
+  var tipo = document.getElementById("tipo").value;
+  var precioPorMetroCuadrado = 0;
+  var metrosCuadrados = 0;
+  var precioTotal = 0;
+  var descuento = 0;
+  var cuotas = 0;
+  var interes = 0;
 
-// Solicitamos los datos al usuario
-alert ("Bienvenido al Simulador de Presupuesto de Cortinas Roller");
-alto = parseFloat(prompt("Ingrese el alto de la cortina (en centimetros):"));
-ancho = parseFloat(prompt("Ingrese el ancho de la cortina (en centimetros):"));
-tipo = prompt("Ingrese el tipo de cortina (Black out o Sun screen):");
-var metodoPago = prompt("Metodos de pago!! \n- Efectivo 10% de DESCUENTO!! \n- 1 Y 3 cuotas SIN INTERES \n- 6 Cuotas 15% de Interes \n- 12 Cuotas 28% de Interes \n\nIngrese el método de pago: \n (Efectivo, 1, 3, 6 o 12 cuotas):");
+  // Calculamos el precio por metro cuadrado según el tipo de cortina
+  if (tipo.toLowerCase() === "blackout") {
+    precioPorMetroCuadrado = 8700;
+  } else if (tipo.toLowerCase() === "sunscreen") {
+    precioPorMetroCuadrado = 9600;
+  } else {
+    document.getElementById("resultado").innerHTML = "Tipo de cortina inválido. Por favor, seleccione Black out o Sun screen.";
+    return;
+  }
 
-// Calculamos el precio por metro cuadrado según el tipo de cortina
-if (tipo.toLowerCase() === "black out") {
-  precioPorMetroCuadrado = 8700;
-} else if (tipo.toLowerCase() === "sun screen") {
-  precioPorMetroCuadrado = 9600;
-} else {
-  alert("Tipo de cortina inválido. Por favor, ingrese Black out o Sun screen.");
+  // Calculamos la cantidad de metros cuadrados
+  metrosCuadrados = (alto / 100) * (ancho / 100);
+
+  // Calculamos el precio total
+  precioTotal = metrosCuadrados * precioPorMetroCuadrado;
+
+  // Calculamos el descuento o interés según el método de pago
+  var metodoPago = document.getElementById("metodoPago").value;
+  if (metodoPago.toLowerCase() === "efectivo") {
+    descuento = precioTotal * 0.1;
+    precioTotal = precioTotal - descuento;
+    document.getElementById("resultado").innerHTML = "Precio Final es de: $" + precioTotal.toFixed(2) + "<br>DESCUENTO: $" + descuento.toFixed(2);
+  } else if (metodoPago === "1" || metodoPago === "3") {
+    descuento = 0;
+    document.getElementById("resultado").innerHTML = "Precio Final es de: $" + precioTotal.toFixed(2);
+  } else if (metodoPago === "6") {
+    interes = precioTotal * 0.15;
+    precioTotal = precioTotal + interes;
+    document.getElementById("resultado").innerHTML = "Precio Final es de: $" + precioTotal.toFixed(2) + "<br>Intereses: $" + interes.toFixed(2);
+  } else if (metodoPago === "12") {
+    interes = precioTotal * 0.28;
+    precioTotal = precioTotal + interes;
+    document.getElementById("resultado").innerHTML = "Precio Final es de: $" + precioTotal.toFixed(2) + "<br>Intereses: $" + interes.toFixed(2);
+  } else {
+    document.getElementById("resultado").innerHTML = "Método de pago inválido. Por favor, seleccione Efectivo, 1, 3, 6 o 12 cuotas.";
+  }
 }
-
-// Calculamos la cantidad de metros cuadrados
-metrosCuadrados = (alto/100)*(ancho/100);
-
-// Calculamos el precio total
-precioTotal = metrosCuadrados * precioPorMetroCuadrado;
-
-// Calculamos el descuento o interés según el método de pago
-if (metodoPago.toLowerCase() === "efectivo") {
-  descuento = precioTotal * 0.1;
-  precioTotal = precioTotal - descuento;
-} else if (metodoPago === "1" || metodoPago === "3") {
-  descuento = 0;
-} else if (metodoPago === "6") {
-  interes = precioTotal * 0.15;
-  precioTotal = precioTotal + interes;
-} else if (metodoPago === "12") {
-  interes = precioTotal * 0.28;
-  precioTotal = precioTotal + interes;
-} else {
-  alert("Método de pago inválido. Por favor, ingrese Efectivo, 1, 3, 6 o 12 cuotas.");
-}
-
-// Mostramos el resultado al usuario
-alert("Precio Final es de: $" + precioTotal.toFixed(2) + "\nInterés: $" + interes.toFixed(2));
